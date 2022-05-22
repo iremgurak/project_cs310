df97bb8902c646a39bc6733e16d47170c7ef682e
Irem Gurak <iremgurak@sabanciuniv.edu>
2022-05-22 18:25:43 +0300

    22/5/2022 irem

--- lib/pages/timeline.dart
+++ lib/pages/timeline.dart
@@ -5,8 +5,10 @@ import 'package:project_cs310/widgets/header.dart';
 import 'package:project_cs310/widgets/progress.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:google_sign_in/google_sign_in.dart';
-
+import 'package:vertical_card_pager/vertical_card_pager.dart';
+import '../models/champion.dart';
 import '../models/user.dart';
+import 'detail_view.dart';
 
 
 final GoogleSignIn googleSignIn = GoogleSignIn();
@@ -50,9 +52,114 @@ class _TimelineState extends State<Timeline> {
 
   @override
   Widget build(context) {
+    final List<String> titles = [
+      "AKALI",
+      "CAMILE",
+      "EZREAL",
+      "IRELIA",
+      "POPPY",
+      "ZOE",
+    ];
+
+    final List<Widget> images = [
+      Hero(
+        tag: "AKALI",
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(20.0),
+          child: Image.asset(
+            "assets/images/akali_lol.gif",
+            fit: BoxFit.cover,
+          ),
+        ),
+      ),
+      Hero(
+        tag: "CAMILE",
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(20.0),
+          child: Image.asset(
+            "assets/images/camile_lol.gif",
+            fit: BoxFit.cover,
+          ),
+        ),
+      ),
+      Hero(
+        tag: "EZREAL",
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(20.0),
+          child: Image.asset(
+            "assets/images/ezreal_lol.gif",
+            fit: BoxFit.cover,
+          ),
+        ),
+      ),
+      Hero(
+        tag: "IRELIA",
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(20.0),
+          child: Image.asset(
+            "assets/images/irelia_lol.gif",
+            fit: BoxFit.cover,
+          ),
+        ),
+      ),
+      Hero(
+        tag: "POPPY",
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(20.0),
+          child: Image.asset(
+            "assets/images/poppy_lol.gif",
+            fit: BoxFit.cover,
+          ),
+        ),
+      ),
+      Hero(
+        tag: "ZOE",
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(20.0),
+          child: Image.asset(
+            "assets/images/zoe_lol.gif",
+            fit: BoxFit.cover,
+          ),
+        ),
+      ),
+    ];
     return Scaffold(
-      appBar: header(context, isAppTitle: true),
-      body: const Text("timeline"),
+      appBar: header(context, titleText: "WasHere", isAppTitle: true),
+      body: SafeArea(
+        child: Column(
+          children: <Widget>[
+            Container(
+              width: double.infinity,
+              height: 70,
+              child: Padding(
+                padding: const EdgeInsets.symmetric(vertical: 15.0),
+              ),
+            ),
+            Expanded(
+              child: Container(
+                child: VerticalCardPager(
+                  // textStyle: TextStyle(color : Colors.red),
+                  titles: titles,
+                  images: images,
+                  onPageChanged: (page) {
+                    // print(page);
+                  },
+                  onSelectedItem: (index) {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => DetailView(
+                            champion:
+                            championsMap[titles[index].toLowerCase()]!,
+                          )),
+                    );
+                  },
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
