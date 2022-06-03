e65d8afd81c52ef6b1d37319cae3fa92b29bbb56
serdaryildizz <94558890+serdaryildizz@users.noreply.github.com>
2022-06-03 06:07:58 +0300

    Update user_preferences.dart

--- lib/utilities/user_preferences.dart
+++ lib/utilities/user_preferences.dart
@@ -6,8 +6,8 @@ class UserPreferences {
         'https://media.istockphoto.com/vectors/user-avatar-profile-icon-black-vector-illustration-vector-id1209654046?k=20&m=1209654046&s=612x612&w=0&h=Atw7VdjWG8KgyST8AXXJdmBkzn0lvgqyWod9vTb2XoE=',
     name: 'Joe Black',
     email: 'joe.black@gmail.com',
-    about:
-        'Dietitian, life coach and restaurateur.',
+    location:
+        'Istanbul/Turkey',
     isDarkMode: false,
   );
 }
