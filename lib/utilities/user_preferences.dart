44f94816bee794071cbceb81d63390e87692d71a
serdaryildizz <94558890+serdaryildizz@users.noreply.github.com>
2022-06-03 05:49:34 +0300

    Create user_preferences.dart

---
+++ lib/utilities/user_preferences.dart
@@ -0,0 +1,13 @@
+import 'package:project_cs310/model/user.dart';
+
+class UserPreferences {
+  static const myUser = User(
+    imagePath:
+        'https://media.istockphoto.com/vectors/user-avatar-profile-icon-black-vector-illustration-vector-id1209654046?k=20&m=1209654046&s=612x612&w=0&h=Atw7VdjWG8KgyST8AXXJdmBkzn0lvgqyWod9vTb2XoE=',
+    name: 'Joe Black',
+    email: 'joe.black@gmail.com',
+    about:
+        'Dietitian, life coach and restaurateur.',
+    isDarkMode: false,
+  );
+}
